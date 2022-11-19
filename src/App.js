434d8bda6e9183e30831bff683177b0d4670312e
Asif Nawaz <assu.nawaz007@gmail.com>
2022-11-20 03:01:59 +0500

    added click event to Button

--- src/App.js
+++ src/App.js
@@ -4,19 +4,24 @@ import {useState} from "react";
 function App() {
  
  const [currentItem, setCurrentItem] = useState(null);
-
+ const [itemList, updateItemList] = useState([])
 
  const onChangeHandler = e => {
   console.log("the CurrentValue", e.target.value);
   setCurrentItem(e.target.value);
- }
+ };
+
+ const addItemToList = () => {
+  updateItemList([...itemList, currentItem]);
+  setCurrentItem("");
+ };
   return (
     <div className="app">
       <header className="app-header">
         <div className="wrapper">
           <div className="input-wrapper">
               <input value = { currentItem } onChange = {onChangeHandler} />
-              <button>+</button>
+              <button onClick={ addItemToList}>+</button>
            </div>
          </div>
       </header>
