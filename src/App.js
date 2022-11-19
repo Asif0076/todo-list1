edaecad6dc454d6048a940ce77d7162ea3ca1f4a
Asif Nawaz <assu.nawaz007@gmail.com>
2022-11-20 02:39:22 +0500

    added useState for Item

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-
 import './App.css';
+import {useState} from "react";
 
 function App() {
+ 
+ const [currentItem, setCurrentItem] = useState(null);
+
+
+ const onChangeHandler = e => {
+  console.log("the CurrentValue", e.target.value);
+  setCurrentItem(e.target.value);
+ }
   return (
     <div className="app">
       <header className="app-header">
         <div className="wrapper">
           <div className="input-wrapper">
-              <input type="text" />
+              <input value = { currentItem } onChange = {onChangeHandler} />
               <button>+</button>
            </div>
          </div>
